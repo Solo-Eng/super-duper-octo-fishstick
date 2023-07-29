d137b63765ef9e73f40f41b305c7b59b994b26c7
SquishedCat <cainl3758@outlook.com>
2023-07-29 17:04:04 -0400

    first commit

---
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -0,0 +1,182 @@
+﻿using ChessChallenge.API;
+using System;
+using System.Linq;
+
+namespace ChessChallenge.Example
+{
+    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
+    // Plays randomly otherwise.
+    public class EvilBot : IChessBot
+    {
+        // Piece values: null, pawn, knight, bishop, rook, queen, king
+        int[] pieceValues = { 0, 100, 350, 350, 600, 1100, 10000 };
+        Random rng = new();
+
+        public Move Think(Board board, Timer timer)
+        {
+            int score = Evaluate(board, 1);
+
+            Move[] moves = board.GetLegalMoves();
+            Move retMove = moves[0];
+
+            foreach (Move move in moves)
+            {
+                board.MakeMove(move);
+                int timeLeft = timer.MillisecondsRemaining;
+                int depth = 4;
+                if (timeLeft < 2000)
+                {
+                    depth--;
+                    if (timeLeft < 1000)
+                    {
+                        depth--;
+                        if (timeLeft < 700)
+                        {
+                            depth--;
+                            if (timeLeft < 500)
+                            {
+                                depth--;
+                            }
+                        }
+                    }
+                }
+                int newScore = AlphaBetaMax(board, timer, int.MinValue, int.MaxValue, depth, -1);
+                board.UndoMove(move);
+                if (newScore < score)
+                {
+                    score = newScore;
+                    retMove = move;
+                }
+            }
+            return retMove;
+        }
+
+        int Evaluate(Board board, int maximizingPlayer)
+        {
+            if (board.IsInCheckmate())
+            {
+                if (maximizingPlayer == -1) return -10000;
+                else return 10000;
+            }
+            if (board.IsDraw()) return 0;
+            int score = 0;
+            PieceList[] pieceLists = board.GetAllPieceLists();
+            for (int i = 0; i < 5; i++)
+            {
+                int val = pieceValues[i + 1];
+                score += (pieceLists[i].Count - pieceLists[i + 6].Count) * val;
+            }
+            return score * (board.IsWhiteToMove ? 1 : -1);
+        }
+
+        int AlphaBetaMax(Board board, Timer timer, int alpha, int beta, int depth_left, int maximizingPlayer)
+        {
+            if (depth_left == 0 || board.IsDraw() || board.IsInCheckmate() || timer.MillisecondsRemaining == 0) return Evaluate(board, maximizingPlayer);
+            Move[] moves = board.GetLegalMoves();
+            foreach (Move move in moves)
+            {
+                board.MakeMove(move);
+                int score = AlphaBetaMax(board, timer, alpha, beta, depth_left - 1, -1 * maximizingPlayer);
+                board.UndoMove(move);
+                if (timer.MillisecondsElapsedThisTurn > 5000) return (maximizingPlayer == 1) ? alpha : beta;
+                if (maximizingPlayer == -1)
+                {
+                    if (score >= beta)
+                    {
+                        return beta;
+                    }
+                    if (score > alpha) alpha = score;
+                }
+                else
+                {
+                    if (score <= alpha)
+                    {
+                        return alpha;
+                    }
+                    if (score < beta) beta = score;
+                }
+            }
+            //reutrns alpha if maximizing, returns beta if not
+            return ((maximizingPlayer == 1) ? beta : alpha);
+        }
+    }
+}
+/*// Piece values: null, pawn, knight, bishop, rook, queen, king
+        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+
+        public Move Think(Board board, Timer timer)
+        {
+            Move[] allMoves = board.GetLegalMoves();
+
+            // Pick a random move to play if nothing better is found
+            Random rng = new();
+            int thatMove = rng.Next(allMoves.Length);
+            Move moveToPlay = allMoves[thatMove];
+
+            int k = 0;
+            do
+            {
+                thatMove = rng.Next(allMoves.Length);
+                if (!board.SquareIsAttackedByOpponent(allMoves[thatMove].TargetSquare)) //something like this but with starting square for the todo
+                {
+                    moveToPlay = allMoves[thatMove];
+                    break;
+                }
+                else
+                {
+                    k++;
+                }
+            }
+            while (k < 1000);
+
+            int highestValueCapture = 0;
+            int highestThreatenedPieceValue = 0;
+
+            foreach (Move move in allMoves)
+            {
+                // Always play checkmate in one
+                if (MoveIsCheckmate(board, move))
+                {
+                    moveToPlay = move;
+                    break;
+                }
+
+                // Find highest value capture
+                Piece capturedPiece = board.GetPiece(move.TargetSquare);
+                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+
+                if ((capturedPieceValue > highestValueCapture) && (capturedPieceValue > highestThreatenedPieceValue))
+                {
+                    if (!board.SquareIsAttackedByOpponent(move.TargetSquare))
+                    {
+                        moveToPlay = move;
+                        highestValueCapture = capturedPieceValue;
+                    }
+                }
+                //ThreatenedPiece
+                Piece movePiece = board.GetPiece(move.StartSquare);
+                int movePieceValue = pieceValues[(int)movePiece.PieceType];
+
+                if ((movePieceValue > highestThreatenedPieceValue) && (movePieceValue > highestValueCapture))
+                {
+                    if (board.SquareIsAttackedByOpponent(move.StartSquare))
+                    {
+                        if (!board.SquareIsAttackedByOpponent(move.TargetSquare))
+                        {
+                            moveToPlay = move;
+                            highestThreatenedPieceValue = movePieceValue;
+                        }
+                    }
+                }
+            }
+            return moveToPlay;
+        }
+
+        // Test if this move gives checkmate
+        bool MoveIsCheckmate(Board board, Move move)
+        {
+            board.MakeMove(move);
+            bool isMate = board.IsInCheckmate();
+            board.UndoMove(move);
+            return isMate;
+        }*/
---
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -0,0 +1,134 @@
+﻿using ChessChallenge.API;
+using System;
+using System.Linq;
+
+public class MyBot : IChessBot
+{
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    int[] pieceValues = { 0, 100, 350, 350, 600, 1100, 10000 };
+    Random rng = new();
+
+    public Move Think(Board board, Timer timer)
+    {
+        int score = getBoardEvaluation(board, true);
+
+        Move[] moves = GetMoves(board);
+        Move retMove = moves[0];
+
+        foreach (Move move in moves)
+        {
+            board.MakeMove(move);
+            int timeLeft = timer.MillisecondsRemaining;
+            int depth = 4;
+            if (timeLeft < 2000)
+            {
+                depth--;
+                if (timeLeft < 1000)
+                {
+                    depth--;
+                    if (timeLeft < 700)
+                    {
+                        depth--;
+                        if (timeLeft < 500)
+                        {
+                            depth--;
+                        }
+                    }
+                }
+            }
+            int newScore = getScore(board, timer, depth, false, -1000000);
+            board.UndoMove(move);
+            if (newScore < score)
+            {
+                score = newScore;
+                retMove = move;
+            }
+        }
+        return retMove;
+    }
+
+    Move[] GetMoves(Board board)
+    {
+        return board.GetLegalMoves().OrderBy(x => rng.Next()).ToArray();
+    }
+
+    int getScore(Board board, Timer timer, int depth, bool playersTurn, int bestScore)
+    {
+        //if the board is in a draw, checkmate, or the depth has run out
+        if (board.IsInCheckmate() || board.IsDraw() || depth == 0)
+        {
+            //evaluate the score
+            //we want to know if it is the player or the opponent's turn
+            return getBoardEvaluation(board, playersTurn);
+        }
+        //otherwise, go another level deep
+        //this means going through all of the children for this move/board state
+        Move[] moves = GetMoves(board);
+        //for each of the childeren, get the score
+        foreach (Move move in moves)
+        {
+            //update the board state for the child 
+            board.MakeMove(move);
+            //score is whatever is gotten
+            int score = getScore(board, timer, depth - 1, !playersTurn, -bestScore); //this is the child
+            //undo the child board state
+            board.UndoMove(move);
+            //check to see how the score measures up with the highest score
+            //if its the players turn, we want a high score so
+            if (playersTurn)
+            {
+                //alpha beta pruning
+                //if it is the players turn, that means the previous player was the opponent
+                //they want the smallest score possible
+                //so if the score is greater than or equal to the best score 
+                //then they won't go down this path
+                //so if bestScore is less than or equal to score
+                if (bestScore < score)
+                {
+                    //purge the result and go to the next possible move
+                    return -bestScore;
+                }
+                //but if it's less, then the opponent will want to go down this path so...
+                //set the new best score to be this score
+                bestScore = Math.Min(score, bestScore);
+            }
+            //otherwise we want the lowest score possible
+            else
+            {
+                //if it is not the players turn, that means the previous player was
+                //so they won't go down this path unless the score is greater than whan they already have
+                //so if the score is less than the best score they won't go down this path
+                if (score < bestScore)
+                {
+                    return -bestScore;
+                }
+                //but if it is more, then they will
+                //set the new best score to be this score
+                bestScore = Math.Max(score, bestScore);
+            }
+        }
+        //now we want to return the bestScore for whoever's turn it is
+        return -bestScore;
+    }
+    int getBoardEvaluation(Board board, bool playersTurn)
+    {
+        int score = 0;
+
+        //get the score by evaluating the pieces on the board
+        PieceList[] pieceLists = board.GetAllPieceLists();
+        for (int i = 0; i < 5; i++)
+        {
+            int val = pieceValues[i + 1];
+            //the pieceList always starts with the white pieces and then does the black pieces
+            score += (pieceLists[i].Count - pieceLists[i + 6].Count) * val;
+            //so if the board is not whites turn, then the score will be inverted
+            //since we want the score in the eyes of whoever's turn it is
+            score = board.IsWhiteToMove ? score : -score;
+        }
+
+        //if it is the players turn we want the score to be positive
+        //otherwise it is negative
+        return playersTurn ? score : -score;
+    }
+}
+
